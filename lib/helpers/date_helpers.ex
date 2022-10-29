8fe8e356389b2c6455e8cdf3e04760a5a80385b6
wagner <wagncarv@outlook.com>
2022-10-29 00:32:43 -0300

    Helper de datas

---
+++ lib/helpers/date_helpers.ex
@@ -0,0 +1,7 @@
+defmodule DateHelpers do 
+def month_to_string(day)  do
+    month = String.to_integer(day) - 1
+    ["janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro"]
+    |> Enum.at(month)
+  end
+end
